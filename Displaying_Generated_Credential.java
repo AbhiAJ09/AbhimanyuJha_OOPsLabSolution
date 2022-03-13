402d85ea83e97c1b1139fd49e988493b66708ae4
Abhimanyu <kumarabhimanyu494@gmail.com>
2022-03-14 00:00:02 +0530

    completed problem statement 1 to 5

---
+++ Displaying_Generated_Credential.java
@@ -0,0 +1,14 @@
+//Program to display generated credentials
+package com.greatlearning.Lab;
+
+public class Displaying_Generated_Credential {
+	public static void main (String [] arg) {
+		//setting password limit
+		int length =8;
+		
+		createPassword obj =new createPassword();
+		
+		//printing password using method described below
+		System.out.println(obj.genPassword(length));
+	}
+}
